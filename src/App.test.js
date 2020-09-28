2d6e239482aa21d9565f12eccf787191506c93dd
dacoldeira <dacoldeira@avansis.com>
2020-09-29 01:03:36 +0200

    Updated

--- src/App.test.js
+++ src/App.test.js
@@ -4,16 +4,10 @@ import App from './App';
 import renderer from 'react-test-renderer';
 import { StateProvider } from './store/store.js';
 
-const globalState={
-  state:{
-      logged:true
-  }
-}
-
 test("renders correctly", () => {
     const tree = renderer
     .create(
-        <StateProvider value={globalState}>
+        <StateProvider>
             <App />
         </StateProvider>
     ).toJSON();
